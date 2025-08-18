e3a49db0172ac68ad7a705dcb9a1ce840bd7972f
Coprodeli Plataforma <coprodelidev@gmail.com>
2025-08-18 17:17:27 +0000

    Corrección vercel 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
+
 export const metadata: Metadata = {
   title: 'Hábitat Next',
   description: 'Descubre las mejores opciones para tu nuevo hogar',
